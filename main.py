194554ef09ead7d4d208a989cf7aea3172226fb0
Shaban245 <shaban.elaev@mail.ru>
2025-09-21 20:49:59 +0300

    update: constants: add Field_constans

--- main.py
+++ main.py
@@ -5,70 +5,36 @@ pygame.init()
 
 # Настройки окна
 WIDTH, HEIGHT = 600, 400
+fps= 1
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Змейка")
-
-# Цвета
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLACK = (0, 0, 0)
-
-# Размер змейки и еды
-BLOCK_SIZE = 20
 clock = pygame.time.Clock()
+snake = pygame.image.load('images/tarzan.jpg')
 
-font = pygame.font.SysFont(None, 35)
 
-def message(msg, color, position):
-    mesg = font.render(msg, True, color)
-    screen.blit(mesg, position)
+x, y = WIDTH / 2, HEIGHT / 2
 
-def game_loop():
-    game_over = False
-    game_close = False
-
-    x1 = WIDTH / 2
-    y1 = HEIGHT / 2
-
-    x1_change = 0
-    y1_change = 0
-
-    snake_List = []
-    Length_of_snake = 1
-
-    foodx = round(random.randrange(0, WIDTH - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE
-    foody = round(random.randrange(0, HEIGHT - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE
-
-    while not game_over:
-
-        while game_close:
-            screen.fill(BLACK)
-            message("Игра окончена! Нажмите Q для выхода или C для повторной игры", RED, [WIDTH / 6, HEIGHT / 3])
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        game_loop()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -BLOCK_SIZE
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = BLOCK_SIZE
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -BLOCK_SIZE
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = BLOCK_SIZE
-                    x1_change = 0
-                    
+while True:
+    pygame.display.update()
+    screen.fill((51, 255 , 51))
+    screen.blit(snake, (x, y))
+    clock.tick(fps)
+    
+    if x > WIDTH or y > HEIGHT:
+        exit()
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                x += 5
+            elif event.key == pygame.K_w:
+                y -= 5
+            elif event.key == pygame.K_s:
+                y += 5
+            elif event.key == pygame.K_a:
+                x -= 5
+    
+    
