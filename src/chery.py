40954fcb095080388aeaf7d62839c7c6f4da38f9
Shaban245 <shaban.elaev@mail.ru>
2025-09-22 21:01:01 +0300

    update: class Chery

--- src/chery.py
+++ src/chery.py
@@ -8,7 +8,11 @@ class Chery:
         self.image = pygame.image.load("images/Chery.jpg")
         self._allowed_x = constants.allowed_chery_x
         self._allowed_y = constants.allowed_chery_y
+        self.x_cor = rd(0, self._allowed_x)
+        self.y_cor = rd(0, self._allowed_y)
         
         
-    def generate_new_position(self) -> list:
-        ...
+    def generate_new_position(self) -> None:
+        self.x_cor = rd(0, self._allowed_x)
+        self.y_cor = rd(0, self._allowed_y)
+        
