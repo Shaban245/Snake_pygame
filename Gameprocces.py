ee4bc64f1baa1bc9972bf24f706625013483c122
Shaban245 <shaban.elaev@mail.ru>
2025-09-24 22:33:06 +0300

    add file Gameprocces and refactor file main

---
+++ Gameprocces.py
@@ -0,0 +1,29 @@
+import pygame
+from src.Game import Game
+from src import constants
+
+
+def main():
+    game = Game()
+
+    while True:
+
+        game.update_screen(game)
+        game.eat_chery(game)
+
+        
+
+        
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    game.snake.change_direction(game.snake.dict_direction[constants.rigth])
+                elif event.key == pygame.K_w:
+                    game.snake.change_direction(game.snake.dict_direction[constants.up])
+                elif event.key == pygame.K_s:
+                    game.snake.change_direction(game.snake.dict_direction[constants.down])
+                elif event.key == pygame.K_a:
+                    game.snake.change_direction(game.snake.dict_direction[constants.left])
